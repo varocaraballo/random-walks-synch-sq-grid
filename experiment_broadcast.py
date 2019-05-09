b7a9b898ab964fb4d2b2761ff93b565dc8f811b3
Luis Evaristo <varo.caraballo87@gmail.com>
2019-05-09 18:31:33 +0200

    Updating broadcast experiment script to get sufficient statistics in less time

--- experiment_broadcast.py
+++ experiment_broadcast.py
@@ -46,7 +46,14 @@ protocol = int(sys.argv[2])
 l = int(sys.argv[3])
 G = grid_graph.diamondGridGraph(n,n)
 startingPositions = [grid_graph.getStartVertexInDiamondGridGraph(i,j) for i in range(n) for j in range(n)]
-broad_casts = np.array([(k, broadcast_experiment(G, startingPositions, k, n*n, protocol, 16*n*n if protocol != drones_team.PR_DETERMNISTIC else min(k*n, 2*n*n), 16*n*n)) for k in range(2,n*n+1) if k <= l])
+ks = [i for i in range(2,n+1)]
+d = [i for i in {int(n*n/j) for j in range(n-1,0,-1)}]
+b = [int((d[i]+d[i+1])/2) for i in range(len(d)-1)]
+ks = sorted(set(ks + d+b))
+#ks = [i for i in range(2,int(n*n/9))]
+#d = [i for i in range(int(n*n/9),n*n,5)]
+#ks = sorted(set(ks + d+[n*n]))
+broad_casts = np.array([(k, broadcast_experiment(G, startingPositions, k, int(5*n*n/k), protocol, 16*n*n if protocol != drones_team.PR_DETERMNISTIC else min(k*n, 2*n*n), 16*n*n)) for k in ks if k <= l])
 
 #f = open("broadcast_{0}_{1}_{2}".format(n,n, 'random' if protocol == drones_team.PR_RANDOM else ('pseudo_random' if protocol == drones_team.PR_PSEUDO_RANDOM else 'deterministic')),"rb")
 #old_data = pickle.load(f)
@@ -55,6 +62,6 @@ broad_casts = np.array([(k, broadcast_experiment(G, startingPositions, k, n*n, p
 #for i in range(len(broad_casts)):
 #    old_data[i] = (old_data[i]+broad_casts[i])/2
 
-f = open("broadcast_{0}_{1}_{2}".format(n,n, 'random' if protocol == drones_team.PR_RANDOM else ('pseudo_random' if protocol == drones_team.PR_PSEUDO_RANDOM else 'deterministic')),"wb")
+f = open("nnn_broadcast_{0}_{1}_{2}".format(n,n, 'random' if protocol == drones_team.PR_RANDOM else ('pseudo_random' if protocol == drones_team.PR_PSEUDO_RANDOM else 'deterministic')),"wb")
 pickle.dump(broad_casts, f)
 f.close()
